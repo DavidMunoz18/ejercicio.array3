bf02a64367ee6fe31cf2ddb158a4cf89c73449d2
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-01-17 11:04:22 +0100

    Add files via upload

---
+++ ejercicio.array3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace ejercicio.array3
+{
+    class program
+    {
+        public static void Main(String[] args) 
+        {
+            int[] arrayEnteros = { 1, 2, 3, 4, 5, 6, 7, 8, };
+
+            ArrayList arrayPaises = new ArrayList();
+
+            string[] provinciasAndalucia = { "Almería", "Córdoba", "Cádiz", "Granada", "Huelva", "Jaén", "Málaga", "Sevilla" };
+            
+            arrayPaises.AddRange(provinciasAndalucia);
+
+            arrayPaises.Sort();
+
+            
+            int x = 0;
+           for(int i = 0;  i < arrayEnteros.Length; i++)
+            {
+                arrayPaises.Insert(x, arrayEnteros[i]);
+                x = x + 2;
+            }
+
+           foreach(var intercalado in  arrayPaises)
+            {
+                Console.WriteLine(intercalado);
+            }
+
+          
+        }
+    }
+}
